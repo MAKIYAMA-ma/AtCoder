0395fadbb50be488f03a6acc2ccc45efd61e69e3
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-09-19 06:50:46 +0900

    now, start to try "AtCorder版蟻本"

---
+++ AntsBook/1-6-1/ARC004A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> x(n);
+    vector<int> y(n);
+    for(int i = 0; i < n; i++) {
+        cin >> x.at(i) >> y.at(i);
+    }
+
+    float ans = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = i; j < n; j++) {
+            float tmp = sqrt(pow())
+        }
+    }
+}
