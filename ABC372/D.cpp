c57fc7242f4e9dba770d07f701791369413098bc
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-09-22 18:44:56 +0900

    question D is cleared

--- ABC372/D.cpp
+++ ABC372/D.cpp
@@ -5,7 +5,7 @@ using ll = long long;
 #define rep(i, n) for(int i = 0; i < (int)(n); i++)
 #define all(vec) vec.begin(), vec.end()
 
-int main() {
+void func1() {
     int n;
     cin >> n;
     vector<int> h(n);
@@ -27,4 +27,37 @@ int main() {
         cout << cnt.at(i) << " ";
     }
     cout << endl;
+}
+
+void func2() {
+    int n;
+    cin >> n;
+    vector<int> h(n);
+    rep(i, n) {
+        cin >> h.at(i);
+    }
+    vector<int> cnt(n, 0);
+    stack<int> j;
+
+    for(int i = n-2; i >= 0; i--) {
+        while(!j.empty()) {
+            auto tmp = j.top();
+            if(h.at(tmp) < h.at(i+1)) {
+                j.pop();
+            } else {
+                break;
+            }
+        }
+        j.push(i+1);
+        cnt.at(i) = j.size();
+    }
+
+    rep(i, n) {
+        cout << cnt.at(i) << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    func2();
 }
