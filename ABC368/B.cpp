4b2e062b0740bff40b398ddfa4cc6a8a59914f3a
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-10-13 20:06:43 +0900

    II have cleared ABC368 A-C

---
+++ ABC368/B.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+using ull = unsigned long long;
+#if 1
+using mint = modint1000000007;
+#else
+using mint = modint998244353;
+#endif
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+
+const ll MAXLL = 1e18;
+const ll MINLL = -1e18;
+const ll MAXLD = 1e18;
+const ll MINLD = -1e18;
+/* const ll MAXLL = numeric_limits<ll>::max(); */
+/* const ll MINLL = numeric_limits<ll>::min(); */
+/* const ll MAXLD = numeric_limits<long double>::max(); */
+/* const ll MINLD = numeric_limits<long double>::min(); */
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    rep(i, n) {
+        cin >> a.at(i);
+    }
+    int ans = 0;
+    sort(rall(a));
+    while(a.at(1) > 0) {
+        a.at(0)--;
+        a.at(1)--;
+        sort(rall(a));
+        ans++;
+    }
+    cout << ans << endl;
+}
