22bd087e29b24b958e2a575415f065d6e5be2d5a
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-10-15 07:13:33 +0900

    Now trying ABC367

---
+++ ABC367/A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+using ull = unsigned long long;
+#if 1
+using mint = modint1000000007;
+#else
+using mint = modint998244353;
+#endif
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+#define Yes(cond) cout << ((cond) ? "Yes" : "No") << endl;
+#define YES(cond) cout << ((cond) ? "YES" : "NO") << endl;
+
+const ll MAXLL = 1e18;
+const ll MINLL = -1e18;
+const ll MAXLD = 1e18;
+const ll MINLD = -1e18;
+/* const ll MAXLL = numeric_limits<ll>::max(); */
+/* const ll MINLL = numeric_limits<ll>::min(); */
+/* const ll MAXLD = numeric_limits<long double>::max(); */
+/* const ll MINLD = numeric_limits<long double>::min(); */
+
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if(b < c) {
+        Yes(a < b || a > c);
+    } else {
+        Yes(a > c && a < b);
+    }
+}
