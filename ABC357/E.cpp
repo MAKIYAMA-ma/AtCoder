18e2817a4acc5284f15f39ae3346830c86325596
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-10-20 10:27:25 +0900

    [cleared] ABC357 E

--- ABC357/E.cpp
+++ ABC357/E.cpp
@@ -11,6 +11,7 @@ using pi = pair<int, int>;
 using vi = vector<int>;
 using vl = vector<ll>;
 using vp = vector<pi>;
+using vb = vector<bool>;
 
 using vi2 = vector<vi>;
 using vl2 = vector<vl>;
@@ -36,33 +37,10 @@ const ll MINLD = -1e18;
 /* const ll MAXLD = numeric_limits<long double>::max(); */
 /* const ll MINLD = numeric_limits<long double>::min(); */
 
-void dijkstra(vector<vector<pair<int, int>>> &graph, int start, vector<int> &cost) {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;   // {cost, index}
-    cost.at(start) = 0;
-    q.push(make_pair(0, start));
-
-    while(!q.empty()) {
-        auto from = q.top().second;
-        auto cst = q.top().first;
-        q.pop();
-
-        if(cost.at(from) < cst) {
-            // 取得した情報が古い（最短でない）
-            continue;
-        }
-        rep(i, graph.at(from).size()) {
-            int to = graph.at(from).at(i).first;
-            int nc = graph.at(from).at(i).second + cst;
-
-            if(nc < cost.at(to)) {
-                cost.at(to) = nc;
-                q.push(make_pair(nc, to));
-            }
-        }
-    }
-}
-
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
     int n;
     cin >> n;
 
@@ -76,31 +54,138 @@ int main() {
     // ある点からDSPする
     // いずれ閉路が見つかる
     // -> 根と閉路の個数を記録する
-#if 1
-    // これはTLE
-    vector<vector<pair<int, int>>> g(n, vector<pair<int, int>>());
+    // 1. 閉路の探索
+    // 2. 閉路の各要素から、有向辺を逆にたどって深さの番号を振っていく
+    // 3. 全部足す
+    vi edge(n, 0);
+    vi2 redge(n, vi());
     rep(i, n) {
         int a;
         cin >> a;
         a--;
-        g[i].push_back({a, 1});
+        edge[i] = a;
+        redge[a].push_back(i);
     }
 
-    vector<int> cost(n, n+1);
-    ll ans = 0;
+    vb chkd(n, false);
+#if 1
+    // cycleの構成要素を探す
+    // O(N)
+    vl cost(n, 0);
+    stack<int> cs;
+    vi cl(n, 0);
     rep(i, n) {
-        vector<int> cost(n, n+1);
-        dijkstra(g, i, cost);
-        ans += count_if(all(cost), [&n](ll x) {
-            return x <= n;
-        });
+        if(chkd[i]) {
+            continue;
+        }
+
+        int pos = i;
+        int cnt = 0;
+        while(!cl[pos]) {
+            cl[pos] = ++cnt;
+            pos = edge[pos];
+        }
+        if(cl[pos] > 0) {
+            int cst = cnt - cl[pos] + 1;
+            while(cost[pos] == 0) {
+                if(redge[pos].size() > 1) {
+                    cs.push(pos);
+                }
+                cost[pos] = cst;
+                pos = edge[pos];
+            }
+        }
+        // 確認済みの頂点を改めて-1に更新する
+        pos = i;
+        while(cl[pos] > 0) {
+            cl[pos] = -1;
+            pos = edge[pos];
+        }
     }
-    cout << ans << endl;
-#else
-    vector<ll> a(n, 0);
+#elif 0
+    // cycleの構成要素を探す
+    // O(N)
+    vl cost(n, 0);
+    stack<int> cs;
+    vi cl(n, 0);
     rep(i, n) {
-        cin >> a[i];
-        a[i]--;
+        if(chkd[i]) {
+            continue;
+        }
+
+        int pos = i;
+        int cnt = 0;
+        // 毎回ゼロのclを用意する処理が重いようだ
+        fill(all(cl), 0);
+        while(!chkd[pos]) {
+            chkd[pos] = true;
+            cl[pos] = ++cnt;
+            pos = edge[pos];
+        }
+        if(cl[pos] > 0) {
+            int cst = cnt - cl[pos] + 1;
+            while(cost[pos] == 0) {
+                if(redge[pos].size() > 1) {
+                    cs.push(pos);
+                }
+                cost[pos] = cst;
+                pos = edge[pos];
+            }
+        }
+    }
+#else
+    // cycleの構成要素を探す
+    // O(N)
+    queue<pi> cp;
+    rep(i, n) {
+        if(chkd[i]) {
+            continue;
+        }
+
+        vi cl(n, 0);
+        int pos = i;
+        int cnt = 0;
+        while(!chkd[pos]) {
+            chkd[pos] = true;
+            cl[pos] = ++cnt;
+            pos = edge[pos];
+        }
+        if(cl[pos] > 0) {
+            cp.push(make_pair(pos, cnt - cl[pos] + 1));
+        }
+    }
+
+    vl cost(n, 0);
+    stack<int> cs;
+    // cycle部分に点数をつけて、チェックスタックに追加
+    // O(N)
+    while(!cp.empty()) {
+        auto tp = cp.front();
+        cp.pop();
+        int pos = tp.first;
+        int cst = tp.second;
+        while(cost[pos] == 0) {
+            if(redge[pos].size() > 1) {
+                cs.push(pos);
+            }
+            cost[pos] = cst;
+            pos = edge[pos];
+        }
     }
 #endif
+
+    // O(N)
+    while(!cs.empty()) {
+        auto tgt = cs.top();
+        cs.pop();
+
+        for(auto nxt: redge[tgt]) {
+            if(cost[nxt] == 0) {
+                cost[nxt] = cost[tgt] + 1;
+                cs.push(nxt);
+            }
+        }
+    }
+
+    cout << reduce(all(cost)) << endl;
 }
