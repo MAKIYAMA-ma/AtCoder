ebb221aef1a6fc3177727662fb736eba6f88404b
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-10-26 09:05:02 +0900

    [cleared] tessoku-book DP A24, A25, B24

---
+++ tessoku/A25.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+#if 1
+using mint = modint1000000007;
+#else
+using mint = modint998244353;
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vm = vector<mint>;
+
+using vi2 = vector<vi>;
+using vl2 = vector<vl>;
+using vpi2 = vector<vpi>;
+using vpl2 = vector<vpl>;
+using vb2 = vector<vb>;
+using vd2 = vector<vd>;
+using vm2 = vector<vm>;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rrep(i, n) for(int i = (int)(n)-1; i >= 0; i--)
+#define srep(i, s, n) for(int i = (int)(s); i < (int)(n); i++)
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+#define Yes(cond) cout << ((cond) ? "Yes" : "No") << endl;
+#define YES(cond) cout << ((cond) ? "YES" : "NO") << endl;
+#define PrintD(val) cout << fixed << setprecision(15) << (val) << endl;
+
+const ll MAXLL = 1e18;
+const ll MINLL = -1e18;
+const ll MAXLD = 1e18;
+const ll MINLD = -1e18;
+const int MAXI = 1e9;
+const int MINI = -1e9;
+/* const ll MAXLL = numeric_limits<ll>::max(); */
+/* const ll MINLL = numeric_limits<ll>::min(); */
+/* const ll MAXLD = numeric_limits<long double>::max(); */
+/* const ll MINLD = numeric_limits<long double>::min(); */
+
+int main() {
+    int h, w;
+    cin >> h >> w;
+
+    vb2 map(h, vb(w, false));
+    rep(i, h) {
+        string s;
+        cin >> s;
+        rep(j, w) {
+            if(s[j] == '.') {
+                map[i][j] = true;
+            }
+        }
+    }
+
+    vl2 dp(h, vl(w, 0));
+    dp[0][0] = 1;
+    rep(i, h) {
+        rep(j, w) {
+            if(i == 0 && j == 0) continue;
+            if(map[i][j]) {
+                dp[i][j] = (i > 0) ? dp[i-1][j] : 0;
+                dp[i][j] += (j > 0) ? dp[i][j-1] : 0;
+            }
+        }
+    }
+    cout << dp[h-1][w-1] << endl;
+}
---
+++ tessoku/B24.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+#if 1
+using mint = modint1000000007;
+#else
+using mint = modint998244353;
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vm = vector<mint>;
+
+using vi2 = vector<vi>;
+using vl2 = vector<vl>;
+using vpi2 = vector<vpi>;
+using vpl2 = vector<vpl>;
+using vb2 = vector<vb>;
+using vd2 = vector<vd>;
+using vm2 = vector<vm>;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rrep(i, n) for(int i = (int)(n)-1; i >= 0; i--)
+#define srep(i, s, n) for(int i = (int)(s); i < (int)(n); i++)
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+#define Yes(cond) cout << ((cond) ? "Yes" : "No") << endl;
+#define YES(cond) cout << ((cond) ? "YES" : "NO") << endl;
+#define PrintD(val) cout << fixed << setprecision(15) << (val) << endl;
+
+const ll MAXLL = 1e18;
+const ll MINLL = -1e18;
+const ll MAXLD = 1e18;
+const ll MINLD = -1e18;
+const int MAXI = 1e9;
+const int MINI = -1e9;
+/* const ll MAXLL = numeric_limits<ll>::max(); */
+/* const ll MINLL = numeric_limits<ll>::min(); */
+/* const ll MAXLD = numeric_limits<long double>::max(); */
+/* const ll MINLD = numeric_limits<long double>::min(); */
+
+int main() {
+    int n;
+    cin >> n;
+
+    vpi xy(n);
+    rep(i, n) cin >> xy[i].first >> xy[i].second;
+    // xを昇順
+    // そのうえで、xが同じもの同士でLISが延びるのを避けるため、
+    // 同一xの範囲では降順ソートする
+    sort(all(xy), [](const pi &a, const pi &b) {
+            if(a.first == b.first) {
+            return a.second > b.second;
+            } else {
+            return a.first < b.first;
+            }
+            });
+    vi dp(n, MAXI);
+    rep(i, n) {
+        auto lp = lower_bound(all(dp), xy[i].second);
+        if(lp != dp.end()) {
+            *lp = xy[i].second;
+        }
+    }
+    cout << lower_bound(all(dp), MAXI) - dp.begin() << endl;
+}
