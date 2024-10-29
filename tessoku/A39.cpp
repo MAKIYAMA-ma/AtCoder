8ec45b76571b58f050ab7f0a510f1186e1c4bf87
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-10-30 06:48:23 +0900

    [cleared] tessoku-book A39

---
+++ tessoku/A39.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+#if 1
+using mint = modint1000000007;
+#else
+using mint = modint998244353;
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vm = vector<mint>;
+
+using vi2 = vector<vi>;
+using vl2 = vector<vl>;
+using vpi2 = vector<vpi>;
+using vpl2 = vector<vpl>;
+using vb2 = vector<vb>;
+using vd2 = vector<vd>;
+using vm2 = vector<vm>;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rrep(i, n) for(int i = (int)(n)-1; i >= 0; i--)
+#define srep(i, s, n) for(int i = (int)(s); i < (int)(n); i++)
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+#define Yes(cond) cout << ((cond) ? "Yes" : "No") << endl;
+#define YES(cond) cout << ((cond) ? "YES" : "NO") << endl;
+#define PrintD(val) cout << fixed << setprecision(15) << (val) << endl;
+
+const ll MAXLL = 1e18;
+const ll MINLL = -1e18;
+const ll MAXLD = 1e18;
+const ll MINLD = -1e18;
+const int MAXI = 1e9;
+const int MINI = -1e9;
+/* const ll MAXLL = numeric_limits<ll>::max(); */
+/* const ll MINLL = numeric_limits<ll>::min(); */
+/* const ll MAXLD = numeric_limits<long double>::max(); */
+/* const ll MINLD = numeric_limits<long double>::min(); */
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vpi lr(n);
+    rep(i, n) cin >> lr[i].first >> lr[i].second;
+    sort(all(lr), [](const pi &a, const pi &b){
+            if(a.second == b.second) {
+            return a.first < b.first;
+            } else {
+            return a.second < b.second;
+            }
+            });
+    int ans = 0;
+    int c = 0;
+    while(c < n) {
+        ans++;
+        int tmp = c+1;
+        while(tmp < n && lr[tmp].first < lr[c].second) tmp++;
+        c = tmp;
+    }
+    cout << ans << endl;
+}
