8cbe7616b6345f23af3dd4f35d7f85495811aa08
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-11-02 14:47:08 +0900

    [cleared] tessoku-book A53, A/B54, A/B55

---
+++ tessoku/B55.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+#if 1
+using mint = modint1000000007;
+#else
+using mint = modint998244353;
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vm = vector<mint>;
+
+using vi2 = vector<vi>;
+using vl2 = vector<vl>;
+using vpi2 = vector<vpi>;
+using vpl2 = vector<vpl>;
+using vb2 = vector<vb>;
+using vd2 = vector<vd>;
+using vm2 = vector<vm>;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rrep(i, n) for(int i = (int)(n)-1; i >= 0; i--)
+#define srep(i, s, n) for(int i = (int)(s); i < (int)(n); i++)
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+#define Yes(cond) cout << ((cond) ? "Yes" : "No") << endl;
+#define YES(cond) cout << ((cond) ? "YES" : "NO") << endl;
+#define PrintD(val) cout << fixed << setprecision(15) << (val) << endl;
+#define Def(n) ll n; cin >> n;
+#define DefA(a, n) vl a(n); rep(i, n) cin >> a[i];
+
+const ll MAXLL = 1e18;
+const ll MINLL = -1e18;
+const ll MAXLD = 1e18;
+const ll MINLD = -1e18;
+const int MAXI = 1e9;
+const int MINI = -1e9;
+/* const ll MAXLL = numeric_limits<ll>::max(); */
+/* const ll MINLL = numeric_limits<ll>::min(); */
+/* const ll MAXLD = numeric_limits<long double>::max(); */
+/* const ll MINLD = numeric_limits<long double>::min(); */
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    Def(q);
+    set<int> st;
+    rep(i, q) {
+        Def(o);
+        Def(x);
+        if(o == 1) {
+            st.insert(x);
+        } else {
+            if(st.size()) {
+                auto lp = st.lower_bound(x);
+                ll ans = MAXLL;
+                if(lp != st.end()) {
+                    ans = *lp - x;
+                }
+                if(lp != st.begin()) {
+                    lp--;
+                    ans = min(ans, x - *lp);
+                }
+                cout << ans << endl;
+            } else {
+                cout << -1 << endl;
+            }
+        }
+    }
+}
