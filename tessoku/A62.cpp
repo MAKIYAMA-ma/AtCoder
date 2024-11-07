629d10aaaeaa4f40358c4560bf30fa6c7a5036ca
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-11-07 21:01:43 +0900

    [cleared] tessoku-book A/B61, A/B62

---
+++ tessoku/A62.cpp
@@ -0,0 +1,155 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+#if 1
+using mint = modint1000000007;
+#else
+using mint = modint998244353;
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vm = vector<mint>;
+
+using vi2 = vector<vi>;
+using vl2 = vector<vl>;
+using vpi2 = vector<vpi>;
+using vpl2 = vector<vpl>;
+using vb2 = vector<vb>;
+using vd2 = vector<vd>;
+using vm2 = vector<vm>;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rrep(i, n) for(int i = (int)(n)-1; i >= 0; i--)
+#define srep(i, s, n) for(int i = (int)(s); i < (int)(n); i++)
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+#define Yes(cond) cout << ((cond) ? "Yes" : "No") << endl;
+#define YES(cond) cout << ((cond) ? "YES" : "NO") << endl;
+#define PrintD(val) cout << fixed << setprecision(15) << (val) << endl;
+#define Def(n) ll n; cin >> n;
+#define DefA(a, n) vl a(n); rep(i, n) cin >> a[i];
+
+const ll MAXLL = 1e18;
+const ll MINLL = -1e18;
+const ll MAXLD = 1e18;
+const ll MINLD = -1e18;
+const int MAXI = 1e9;
+const int MINI = -1e9;
+/* const ll MAXLL = numeric_limits<ll>::max(); */
+/* const ll MINLL = numeric_limits<ll>::min(); */
+/* const ll MAXLD = numeric_limits<long double>::max(); */
+/* const ll MINLD = numeric_limits<long double>::min(); */
+
+class UnionFind {
+    public:
+        vector<int> data;
+
+        void init(int n) {
+            data.resize(n, -1);
+        }
+
+        int root(int idx) {
+            if(data.at(idx) == -1) {
+                return idx;
+            } else {
+                // 深さ圧縮
+                // ただし、同じグループの全要素の値が同じになるとは限らないので注意
+                // (idxより根に近い要素の値は書き換わるが、idxより下の要素の値は書き換わらない。
+                // その結果、2段にはなりうる。
+                return (data.at(idx) = root(data.at(idx)));
+            }
+        }
+
+        void merge(int a, int b) {
+            int ar, br;
+
+            if(a == b) {
+                return;
+            }
+
+            ar = root(a);
+            br = root(b);
+            if(ar != br) {
+                data.at(br) = ar;
+            }
+        }
+
+        bool same(int a, int b) {
+            int ar, br;
+
+            if(a == b) {
+                return true;
+            }
+
+            ar = root(a);
+            br = root(b);
+
+            return (ar == br);
+        }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    Def(n);
+    Def(m);
+#if 1
+    vl2 edge(n, vl());
+    rep(i, m) {
+        Def(a);
+        Def(b);
+        a--;
+        b--;
+        edge[a].push_back(b);
+        edge[b].push_back(a);
+    }
+    stack<ll> st;
+    vb visited(n, false);
+    st.push(0);
+    while(!st.empty()) {
+        auto pt = st.top();
+        st.pop();
+        visited[pt] = true;
+        rep(i, edge[pt].size()) {
+            if(!visited[edge[pt][i]]) st.push(edge[pt][i]);
+        }
+    }
+    rep(i, n) {
+        if(!visited[i]) {
+            cout << "The graph is not connected." << endl;
+            return 0;
+        }
+    }
+    cout << "The graph is connected." << endl;
+#else
+    UnionFind uf;
+    uf.init(n);
+
+    rep(i, m) {
+        Def(a);
+        Def(b);
+        uf.merge(a-1, b-1);
+    }
+    rep(i, n) {
+        if(!uf.same(0, i)) {
+            cout << "The graph is not connected." << endl;
+            return 0;
+        }
+    }
+    cout << "The graph is connected." << endl;
+#endif
+}
