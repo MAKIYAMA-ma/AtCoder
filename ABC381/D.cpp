17b960a772227bdafecc6fc0dacafaf87e717222
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-11-22 22:45:34 +0900

    [contest] ABC381 (D is not cleared)

---
+++ ABC381/D.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+#if 1
+using mint = modint1000000007;
+#else
+using mint = modint998244353;
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vm = vector<mint>;
+using vs = vector<string>;
+
+using vi2 = vector<vi>;
+using vl2 = vector<vl>;
+using vpi2 = vector<vpi>;
+using vpl2 = vector<vpl>;
+using vb2 = vector<vb>;
+using vd2 = vector<vd>;
+using vm2 = vector<vm>;
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define rrep(i, n) for(ll i = (ll)(n)-1; i >= 0; i--)
+#define srep(i, s, n) for(ll i = (ll)(s); i < (ll)(n); i++)
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+#define Yes(cond) cout << ((cond) ? "Yes" : "No") << endl;
+#define YES(cond) cout << ((cond) ? "YES" : "NO") << endl;
+#define PrintD(val) cout << fixed << setprecision(15) << (val) << endl;
+#define Def(n) ll n; cin >> n;
+#define DefA(a, n) vl a(n); rep(i, n) cin >> a[i];
+
+const ll MAXLL = 1e18;
+const ll MINLL = -1e18;
+const ll MAXLD = 1e18;
+const ll MINLD = -1e18;
+const int MAXI = 1e9;
+const int MINI = -1e9;
+/* const ll MAXLL = numeric_limits<ll>::max(); */
+/* const ll MINLL = numeric_limits<ll>::min(); */
+/* const ll MAXLD = numeric_limits<long double>::max(); */
+/* const ll MINLD = numeric_limits<long double>::min(); */
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    Def(n);
+    vl a(n+1);
+    rep(i, n) {
+        cin >> a[i];
+    }
+    a[n] = n+1;
+    // 11223344 44
+    ll ans{0};
+    ll st{0};
+    vl pos(n+2, 0);
+    for(ll i = 0; i < n;) {
+        if(a[i] != a[i+1]) {
+            ans = max(ans, i - st);
+            if(i > 0 && (a[i] == a[i-1])) {
+                pos[a[i]] = i;
+                st = i-1;
+            } else {
+                st = i+1;
+            }
+            i++;
+        } else if(pos[a[i]] >= st) {
+            ans = max(ans, i - st);
+            st = pos[a[i]] + 2;
+            pos[a[i]] = i;
+            i += 2;
+        } else {
+            pos[a[i]] = i;
+            i += 2;
+        }
+    }
+    ans = max(ans, n - st);
+    cout << ans << endl;
+}
