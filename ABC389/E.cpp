555ad95165375069d81a60083e485873c8fb21cf
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2025-01-19 10:03:17 +0900

    [trying] ABC389E

--- ABC389/E.cpp
+++ ABC389/E.cpp
@@ -64,21 +64,70 @@ int main() {
     Def(m);
     DefA(p, n);
 
+#if 1
+    ll l{0}, r{MAXLL+1};
+    while(r-l > 1) {
+        ll mid = (l+r)/2;
+        ll t{0};
+        rep(i, n) {
+            ll nm = ((float)mid/p[i]-1)/2;
+            t += (nm*nm*p[i]);
+            if(t > MAXLL) break;
+        }
+        if(t <= m) {
+            l = mid;
+        } else {
+            r = mid;
+        }
+    }
+
+    ll t{0};
+    ll ans{0};
+    priority_queue<ll, vl, greater<ll>> pq;
+
+    rep(i, n) {
+        ll nm = ((float)l/p[i]-1)/2;
+        t += (nm*nm*p[i]);
+        ans += nm;
+        pq.push((2*nm+1)*p[i]);
+        cout << nm << " ";
+    }
+    cout << endl;
+    cout << "DB:" << ans << " " << t << endl;
+    while(!pq.empty()) {
+        auto tp = pq.top();
+        pq.pop();
+        cout << m << " " << t << " " << tp << endl;
+        if(t+tp <= m) {
+            ans++;
+            t += tp;
+        } else {
+            break;
+        }
+    }
+    cout << ans << endl;
+#else
     priority_queue<pair<ll, pl>, vector<pair<ll, pl>>, greater<pair<ll, pl>>> pq;
     rep(i, n) {
         pq.push(make_pair(p[i], make_pair(1, i)));
     }
     ll mn{0};
     ll ans{0};
+    map<ll, ll> mp;
     while(mn <= m) {
         auto t = pq.top();
         pq.pop();
         if(mn + t.first > m) break;
+        /* cout << "DB:" << t.first << endl; */
         ans++;
         mn += t.first;
         auto c = t.second.first;
         auto n = t.second.second;
+        mp[n]++;
         pq.push(make_pair(((c+1)*(c+1)-c*c)*p[n], make_pair(c+1, n)));
     }
+    rep(i, n) cout << "(" << mp[i] << "," << (mp[i]*mp[i]*p[i]) << ") ";
+    cout << endl;
     cout<< ans << endl;
+#endif
 }
