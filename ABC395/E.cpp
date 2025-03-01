7f8dd9c0a915395486de9a05e7b587c3470f106c
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2025-03-01 22:32:48 +0900

    [contest] ABC395

---
+++ ABC395/E.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+#if 1
+using mint = modint1000000007;
+#else
+using mint = modint998244353;
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vm = vector<mint>;
+using vs = vector<string>;
+
+using vi2 = vector<vi>;
+using vl2 = vector<vl>;
+using vpi2 = vector<vpi>;
+using vpl2 = vector<vpl>;
+using vb2 = vector<vb>;
+using vd2 = vector<vd>;
+using vm2 = vector<vm>;
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define rrep(i, n) for(ll i = (ll)(n)-1; i >= 0; i--)
+#define srep(i, s, n) for(ll i = (ll)(s); i < (ll)(n); i++)
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+#define Yes(cond) cout << ((cond) ? "Yes" : "No") << endl;
+#define YES(cond) cout << ((cond) ? "YES" : "NO") << endl;
+#define PrintD(val) cout << fixed << setprecision(15) << (val) << endl;
+#define Def(n) ll n; cin >> n;
+#define DefA(a, n) vl a(n); rep(i, n) cin >> a[i];
+#define Def2A(a, b, n) vl a(n); vl b(n); rep(i, n) cin >> a[i] >> b[i];
+#define DefPA(a, n) vl a(n); rep(i, n) cin >> a[i].first >> a[i].second;
+
+const ll MAXLL = 1e18;
+const ll MINLL = -1e18;
+const ll MAXLD = 1e18;
+const ll MINLD = -1e18;
+const int MAXI = 1e9;
+const int MINI = -1e9;
+/* const ll MAXLL = numeric_limits<ll>::max(); */
+/* const ll MINLL = numeric_limits<ll>::min(); */
+/* const ll MAXLD = numeric_limits<long double>::max(); */
+/* const ll MINLD = numeric_limits<long double>::min(); */
+
+
+void dijkstra(vpl2 &graph, ll start, vl &cost) {
+    priority_queue<pl, vpl, greater<pl>> q;   // {cost, index}
+    cost.at(start) = 0;
+    q.push(make_pair(0, start));
+
+    while(!q.empty()) {
+        auto from = q.top().second;
+        auto cst = q.top().first;
+        q.pop();
+
+        if(cost.at(from) < cst) {
+            // 取得した情報が古い（最短でない）
+            continue;
+        }
+        rep(i, graph.at(from).size()) {
+            int to = graph.at(from).at(i).first;
+            ll nc = graph.at(from).at(i).second + cst;
+
+            if(nc < cost.at(to)) {
+                cost.at(to) = nc;
+                q.push(make_pair(nc, to));
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    Def(n);
+    Def(m);
+    Def(x);
+    vpl2 g(n*2, vpl());
+    rep(i, m) {
+        Def(u);
+        Def(v);
+        u--; v--;
+        g[u].push_back({v, 1});
+        g[v+n].push_back({u+n, 1});
+        g[u+n].push_back({v, x+1});
+        g[v].push_back({u+n, x+1});
+        /* g[v].push_back({u+n, x+1}); */
+        /* g[u+n].push_back({v, x+1}); */
+        /* g[v+n].push_back({u+n, 1}); */
+    }
+    /* rep(i, g.size()) { */
+    /*     cout << i << ":"; */
+    /*     rep(j, g[i].size()) { */
+    /*         cout << "(" << g[i][j].first << "," << g[i][j].second << ")"; */
+    /*     } */
+    /*     cout << endl; */
+    /* } */
+
+    vl cost(n*2, MAXLL);
+    dijkstra(g, 0, cost);
+    ll ans = min(cost[n-1], cost[n*2-1]);
+    vl cost2(n*2, MAXLL);
+    dijkstra(g, n, cost2);
+    ans = min({ans, x+cost2[n-1], x+cost2[n*2-1]});
+    cout << ans << endl;
+}
