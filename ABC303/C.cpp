f647ad621153a834b5a6c99e8bc1978c2bc12ead
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2025-03-16 09:08:36 +0900

    [cleared] ABC303A-C

---
+++ ABC303/C.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+#if 1
+using mint = modint1000000007;
+#else
+using mint = modint998244353;
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vm = vector<mint>;
+using vs = vector<string>;
+
+using vi2 = vector<vi>;
+using vl2 = vector<vl>;
+using vpi2 = vector<vpi>;
+using vpl2 = vector<vpl>;
+using vb2 = vector<vb>;
+using vd2 = vector<vd>;
+using vm2 = vector<vm>;
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define rrep(i, n) for(ll i = (ll)(n)-1; i >= 0; i--)
+#define srep(i, s, n) for(ll i = (ll)(s); i < (ll)(n); i++)
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+#define Yes(cond) cout << ((cond) ? "Yes" : "No") << endl;
+#define YES(cond) cout << ((cond) ? "YES" : "NO") << endl;
+#define PrintD(val) cout << fixed << setprecision(15) << (val) << endl;
+#define Def(n) ll n; cin >> n;
+#define Def2(m, n) ll m, n; cin >> m >> n;
+#define Def3(l, m, n) ll l, m, n; cin >> l >> m >> n;
+#define DefA(a, n) vl a(n); rep(i, n) cin >> a[i];
+#define Def2A(a, b, n) vl a(n); vl b(n); rep(i, n) cin >> a[i] >> b[i];
+#define DefPA(a, n) vl a(n); rep(i, n) cin >> a[i].first >> a[i].second;
+
+const ll MAXLL = 1e18;
+const ll MINLL = -1e18;
+const ll MAXLD = 1e18;
+const ll MINLD = -1e18;
+const int MAXI = 1e9;
+const int MINI = -1e9;
+/* const ll MAXLL = numeric_limits<ll>::max(); */
+/* const ll MINLL = numeric_limits<ll>::min(); */
+/* const ll MAXLD = numeric_limits<long double>::max(); */
+/* const ll MINLD = numeric_limits<long double>::min(); */
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    Def2(n, m);
+    Def2(h, k);
+    string s;
+    cin >> s;
+    map<ll, set<ll>> itm;
+    rep(i, m) {
+        Def2(x, y);
+        itm[x].insert(y);
+    }
+
+    vl dx = {1, -1, 0, 0};
+    vl dy = {0, 0, 1, -1};
+    ll x{0}, y{0};
+    bool ans{true};
+    rep(i, n) {
+        ll dir{0};
+        switch(s[i]) {
+            case 'R': dir = 0; break;
+            case 'L': dir = 1; break;
+            case 'U': dir = 2; break;
+            case 'D': dir = 3; break;
+        }
+        x += dx[dir];
+        y += dy[dir];
+        h--;
+        if(h < 0) {
+            ans = false;
+            break;
+        }
+        if(itm[x].find(y) != itm[x].end()) {
+            if(h < k) {
+                h = k;
+                itm[x].erase(y);
+            }
+        }
+    }
+    Yes(ans);
+}
