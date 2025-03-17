b8300e9f7ec2b0d2c5019470b71047ad50f3216b
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2025-03-17 20:11:42 +0900

    [cleared] ABC302A-F

---
+++ ABC302/F.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+#if 1
+using mint = modint1000000007;
+#else
+using mint = modint998244353;
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+using sl = set<ll>;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vm = vector<mint>;
+using vs = vector<string>;
+using vsl = vector<sl>;
+
+using vi2 = vector<vi>;
+using vl2 = vector<vl>;
+using vpi2 = vector<vpi>;
+using vpl2 = vector<vpl>;
+using vb2 = vector<vb>;
+using vd2 = vector<vd>;
+using vm2 = vector<vm>;
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define rrep(i, n) for(ll i = (ll)(n)-1; i >= 0; i--)
+#define srep(i, s, n) for(ll i = (ll)(s); i < (ll)(n); i++)
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+#define Yes(cond) cout << ((cond) ? "Yes" : "No") << endl;
+#define YES(cond) cout << ((cond) ? "YES" : "NO") << endl;
+#define PrintD(val) cout << fixed << setprecision(15) << (val) << endl;
+#define Def(n) ll n; cin >> n;
+#define Def2(m, n) ll m, n; cin >> m >> n;
+#define Def3(l, m, n) ll l, m, n; cin >> l >> m >> n;
+#define DefA(a, n) vl a(n); rep(i, n) cin >> a[i];
+#define Def2A(a, b, n) vl a(n); vl b(n); rep(i, n) cin >> a[i] >> b[i];
+#define DefPA(a, n) vl a(n); rep(i, n) cin >> a[i].first >> a[i].second;
+
+const ll MAXLL = 1e18;
+const ll MINLL = -1e18;
+const ll MAXLD = 1e18;
+const ll MINLD = -1e18;
+const int MAXI = 1e9;
+const int MINI = -1e9;
+/* const ll MAXLL = numeric_limits<ll>::max(); */
+/* const ll MINLL = numeric_limits<ll>::min(); */
+/* const ll MAXLD = numeric_limits<long double>::max(); */
+/* const ll MINLD = numeric_limits<long double>::min(); */
+
+void dijkstra(vpl2 &graph, ll start, vl &cost) {
+    priority_queue<pl, vpl, greater<pl>> q;   // {cost, index}
+    cost.at(start) = 0;
+    q.push(make_pair(0, start));
+
+    while(!q.empty()) {
+        auto from = q.top().second;
+        auto cst = q.top().first;
+        q.pop();
+
+        if(cost.at(from) < cst) {
+            // 取得した情報が古い（最短でない）
+            continue;
+        }
+        rep(i, graph.at(from).size()) {
+            int to = graph.at(from).at(i).first;
+            ll nc = graph.at(from).at(i).second + cst;
+
+            if(nc < cost.at(to)) {
+                cost.at(to) = nc;
+                q.push(make_pair(nc, to));
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    Def2(n, m);
+    // n, m <= 2*10^5
+    // ただし全集合の要素数は<=5*10^5
+
+    map<ll, sl> mp;
+    rep(i, n) {
+        Def(a);
+        rep(j, a) {
+            Def(s);
+            mp[s].insert(i);
+        }
+    }
+
+    vpl2 grp(n+m, vpl());
+    // これなら全要素数x2本しか枝を張らないからTLEしない
+    for(auto [s, lst] : mp) {
+        for(auto v : lst) {
+            grp[v].push_back({n+s-1, 0});
+            grp[n+s-1].push_back({v, 1});
+        }
+    }
+    vl cost(n+m, MAXLL);
+    dijkstra(grp, n, cost);
+    /* rep(i, cost.size()) cout << cost[i] << " "; */
+    /* cout << endl; */
+    // 最初に同じ集合内で別の数字に移動する際にカウントされるコスト+1は減じる
+    cout << ((cost[n+m-1] == MAXLL)?(-1):(cost[n+m-1]-1)) << endl;
+}
