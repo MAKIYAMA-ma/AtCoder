2f53686142bd05fa134ac17b265535d92b9e9859
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2025-03-19 06:21:11 +0900

    [cleared] ABC301C

---
+++ ABC301/C.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+#if 1
+using mint = modint1000000007;
+#else
+using mint = modint998244353;
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+using sl = set<ll>;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vm = vector<mint>;
+using vs = vector<string>;
+using vsl = vector<sl>;
+
+using vi2 = vector<vi>;
+using vl2 = vector<vl>;
+using vpi2 = vector<vpi>;
+using vpl2 = vector<vpl>;
+using vb2 = vector<vb>;
+using vd2 = vector<vd>;
+using vm2 = vector<vm>;
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define rrep(i, n) for(ll i = (ll)(n)-1; i >= 0; i--)
+#define srep(i, s, n) for(ll i = (ll)(s); i < (ll)(n); i++)
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+#define Yes(cond) cout << ((cond) ? "Yes" : "No") << endl;
+#define YES(cond) cout << ((cond) ? "YES" : "NO") << endl;
+#define PrintD(val) cout << fixed << setprecision(15) << (val) << endl;
+#define Def(n) ll n; cin >> n;
+#define Def2(m, n) ll m, n; cin >> m >> n;
+#define Def3(l, m, n) ll l, m, n; cin >> l >> m >> n;
+#define DefA(a, n) vl a(n); rep(i, n) cin >> a[i];
+#define Def2A(a, b, n) vl a(n); vl b(n); rep(i, n) cin >> a[i] >> b[i];
+#define DefPA(a, n) vl a(n); rep(i, n) cin >> a[i].first >> a[i].second;
+
+const ll MAXLL = 1e18;
+const ll MINLL = -1e18;
+const ll MAXLD = 1e18;
+const ll MINLD = -1e18;
+const int MAXI = 1e9;
+const int MINI = -1e9;
+/* const ll MAXLL = numeric_limits<ll>::max(); */
+/* const ll MINLL = numeric_limits<ll>::min(); */
+/* const ll MAXLD = numeric_limits<long double>::max(); */
+/* const ll MINLD = numeric_limits<long double>::min(); */
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    set<char> chg = {'a', 't', 'c', 'o', 'd', 'e', 'r'};
+    string s, t;
+    cin >> s >> t;
+    map<char, ll> ms, mt;
+    set<char> cd;
+    rep(i, s.length()) {
+        ms[s[i]]++;
+        cd.insert(s[i]);
+    }
+    rep(i, t.length()) {
+        mt[t[i]]++;
+        cd.insert(t[i]);
+    }
+
+    for(auto k : cd) {
+        if(k == '@') continue;
+        if(chg.find(k) != chg.end()) {
+            if(mt[k] > ms[k]) {
+                ms['@'] -= (mt[k]-ms[k]);
+            } else {
+                mt['@'] -= (ms[k]-mt[k]);
+            }
+            mt[k] = 0;
+            ms[k] = 0;
+            if(mt['@'] < 0 || ms['@'] < 0) {
+                Yes(0);
+                return 0;
+            }
+        } else {
+            if(ms[k] != mt[k]) {
+                Yes(0);
+                return 0;
+            }
+        }
+    }
+    Yes(1);
+}
