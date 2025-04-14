423d468aec3451401c3c83fb1f43a91249dbff8d
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2025-04-14 20:23:05 +0900

    [cleared] ABC401E

--- ABC401/E.cpp
+++ ABC401/E.cpp
@@ -65,4 +65,118 @@ const int MINI = -1e9;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
+
+    Def2(n, m);
+    vl2 edge(n, vl());
+    rep(i, m) {
+        Def2(u, v);
+        u--; v--;
+        edge[u].push_back(v);
+        edge[v].push_back(u);
+    }
+    rep(i, n) sort(all(edge[i]));
+
+#if 1
+    vb rch(n, false);
+    unordered_set<ll> pool; // 現状到達不能の点
+    rch[0] = true;
+    srep(i, 1, n) {
+        // iが0から到達可能かチェック
+        bool ok{false};
+        rep(j, edge[i].size()) {
+            if(edge[i][j] > i) break;
+            if(pool.find(edge[i][j]) == pool.end()) {
+                ok = true;
+                break;
+            }
+        }
+        if(ok) {
+            // iが0から到達可能なら、iから到達可能な点をpoolから除去
+            queue<ll> q;
+            rep(j, edge[i].size()) {
+                if(edge[i][j] > i) break;
+                q.push(edge[i][j]);
+            }
+            while(!q.empty()) {
+                auto tp = q.front();
+                q.pop();
+                if(pool.find(tp) != pool.end()) {
+                    pool.erase(tp);
+                    rep(j, edge[tp].size()) {
+                        if(edge[tp][j] > i) break;
+                        q.push(edge[tp][j]);
+                    }
+                }
+            }
+        } else {
+            pool.insert(i);
+        }
+        // 到達不能なi以下の点がないならOK
+        rch[i] = (pool.size() == 0);
+    }
+#else
+    // TODO
+    // iが1から到達可能、
+    // かつiに直結されていないj < iについて、すべてのjが到達可能である必要がある
+    // でもこれ普通にチェックしようとするとTLEでは？
+    // 到達可能か判定
+    vb rch(n, false);
+    rch[0] = true;
+    srep(i, 1, n) {
+        rep(j, edge[i].size()) {
+            if(edge[i][j] > i) break;
+            if(rch[edge[i][j]]) {
+                rch[i] = true;
+                break;
+            }
+        }
+    }
+
+    vb clr(n, false);
+    clr[0] = true;
+    sl pool; // 現状到達不能の点
+    rep(i, n) {
+        if(!rch[i]) {
+            pool.insert(i);
+        } else {
+            queue<ll> q;
+            rep(j, edge[i].size()) {
+                if(edge[i][j] > i) break;
+                q.push(edge[i][j]);
+            }
+            while(!q.empty()) {
+                auto tp = q.front();
+                q.pop();
+                if(pool.find(tp) != pool.end()) {
+                    pool.erase(tp);
+                    rep(j, edge[tp].size()) {
+                        if(edge[tp][j] > i) break;
+                        q.push(edge[tp][j]);
+                    }
+                }
+            }
+        }
+        clr[i] = (pool.size() == 0);
+    }
+#endif
+
+    vl ans(n, 0);
+    srep(i, 1, n) {
+        // iに接続された最小の点jがi以下である場合、jまで残す場合~i-1まで残すまでの場合の間は、
+        // iは削除が必要
+        if(edge[i].size() > 0 && i >= edge[i][0]) {
+            ans[edge[i][0]]++;
+            ans[i]--;
+        }
+    }
+    srep(i, 1, n) {
+        ans[i] += ans[i-1];
+    }
+    rep(i, n) {
+        if(!rch[i]) {
+            cout << -1 << endl;
+        } else {
+            cout << ans[i] << endl;
+        }
+    }
 }
