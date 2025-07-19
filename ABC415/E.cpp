6e2c79e1e96169a0df91430ba18a6139f3602018
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2025-07-19 22:42:23 +0900

    [contest] ABC415 (E is not cleared)

---
+++ ABC415/E.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+#if 1
+using mint = modint1000000007;
+#else
+using mint = modint998244353;
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+using sl = set<ll>;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vm = vector<mint>;
+using vs = vector<string>;
+using vsl = vector<sl>;
+
+using vi2 = vector<vi>;
+using vl2 = vector<vl>;
+using vpi2 = vector<vpi>;
+using vpl2 = vector<vpl>;
+using vb2 = vector<vb>;
+using vd2 = vector<vd>;
+using vm2 = vector<vm>;
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define rrep(i, n) for(ll i = (ll)(n)-1; i >= 0; i--)
+#define srep(i, s, n) for(ll i = (ll)(s); i < (ll)(n); i++)
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+#define Yes(cond) cout << ((cond) ? "Yes" : "No") << endl;
+#define YES(cond) cout << ((cond) ? "YES" : "NO") << endl;
+#define PrintD(val) cout << fixed << setprecision(15) << (val) << endl;
+#define Def(n) ll n; cin >> n;
+#define Def2(m, n) ll m, n; cin >> m >> n;
+#define Def3(l, m, n) ll l, m, n; cin >> l >> m >> n;
+#define DefA(a, n) vl a(n); rep(i, n) cin >> a[i];
+#define Def2A(a, b, n) vl a(n); vl b(n); rep(i, n) cin >> a[i] >> b[i];
+#define Def3A(a, b, c, n) vl a(n); vl b(n); vl c(n); rep(i, n) cin >> a[i] >> b[i] >> c[i];
+#define DefPA(a, n) vl a(n); rep(i, n) cin >> a[i].first >> a[i].second;
+
+const ll MAXLL = 1e18;
+const ll MINLL = -1e18;
+const ll MAXLD = 1e18;
+const ll MINLD = -1e18;
+const int MAXI = 1e9;
+const int MINI = -1e9;
+/* const ll MAXLL = numeric_limits<ll>::max(); */
+/* const ll MINLL = numeric_limits<ll>::min(); */
+/* const ll MAXLD = numeric_limits<long double>::max(); */
+/* const ll MINLD = numeric_limits<long double>::min(); */
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    Def2(h, w);
+    vl2 a(h, vl(w));
+    rep(i, h) rep(j, w) cin >> a[i][j];
+    DefA(p, h+w-1);
+
+    // 各マスに到達する時点で、i点もって到達するためには、そこまでの最小コインがj
+    // たぶんＤＰ？　でもコインは10^9のオーダー
+    // mapで持ったら？
+    vector<map<ll, ll>> mp(h*w, map<ll, ll>());
+    mp[0][a[0][0]] = a[0][0]-p[0];
+    queue<ll> q;
+    q.push(0);
+    while(!q.emtpy()) {
+        auto tp = q.top();
+        q.pop();
+
+        ll ch = tp/w;
+        ll cw = tp%w;
+        if(ch == h-1 && cw == w-1) break;
+        if(ch+1 > h) {
+            ll pt = a[ch+1][cw]-p[ch+cw+1];
+            for(auto [p1, p2] : mp[w*ch+cw]) {
+                mp[w*(ch+1)+cw][p1+pt] = 
+            }
+        }
+    }
+}
